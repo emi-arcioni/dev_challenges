889c6cb0278918f517bf1fde14a023c5d8f68a80
Emilio Arcioni <emilio@arcioni.com.ar>
2021-04-13 02:00:10 -0300

    Error handler implementation

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,15 +3,31 @@ declare(strict_types=1);
 
 
 use Slim\Factory\AppFactory;
+use DI\Container;
+use App\Handlers\HttpErrorHandler;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$container = new Container();
+$app = AppFactory::createFromContainer($container);
+
+$callableResolver = $app->getCallableResolver();
+$responseFactory = $app->getResponseFactory();
 
 $middleware = require __DIR__ . '/../app/middleware.php';
 $middleware($app);
 
 $routes = require __DIR__ . '/../app/routes.php';
 $routes($app);
+
+$errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
+
+// Add Routing Middleware
+$app->addRoutingMiddleware();
+
+// Add Error Handling Middleware
+$errorMiddleware = $app->addErrorMiddleware(true, false, false);
+$errorMiddleware->setDefaultErrorHandler($errorHandler);
+
 $app->run();
 
