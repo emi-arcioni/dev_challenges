9992f3bf3b0ac769068785676a80079d05269a41
Emilio Arcioni <emilio@arcioni.com.ar>
2021-04-14 21:43:04 -0300

    Fix response

--- backend/app/Controllers/IssueController.php
+++ backend/app/Controllers/IssueController.php
@@ -106,7 +106,7 @@ class IssueController extends Controller
         if (!$member_exists) {
             $members[] = $member;
         } else {
-            return $this->response(['message' => $member['name'] . ' already joined the issue #' . $id], $resp);
+            throw new HttpBadRequestException($request, $member['name'] . ' already joined the issue #' . $id);
         }
         $this->db->redis->hmset($id, ['status' => 'voting', 'members' => json_encode($members)]);
 
